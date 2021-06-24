0e48901942211f1859e056116cba6f076b42e717
lovecosma <williamsmatthew333@gmail.com>
2021-06-23 20:07:14 -0600

    commit tree 56eb02567ce9b171a0fa4f8c0db2a0470c7f64df

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,25 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+    counter = 1
+    numbers = []
+    while counter < limit 
+        if counter % 3 == 0 || counter % 5 == 0
+            numbers << counter 
+        end 
+        counter += 1
+    end
+    numbers
+end 
+
+def sum_multiples(limit)
+    counter = 1
+    sum = 0
+    while counter < limit 
+        if counter % 3 == 0 || counter % 5 == 0
+            sum += counter 
+        end 
+        counter += 1
+    end
+    sum
+end 
